e4d27ecaba1cab81db6530f08250c95e7357af8b
Shiven48 <shevenroyal@gmail.com>
2025-02-13 22:46:16 +0530

    Deleted some pages and solved routing problem

--- app/(ROOT)/pages/categories/[category]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Page({params}:{params:{category:string}}){
-    const category = decodeURIComponent((await params)['category'])
-    console.log(category)
-    return(
-        <>
-            Hello this is {category}
-        </>
-    )
-}
