24d4be9e07f5a7b10e5898978e62f7569962451b
Shiven48 <shevenroyal@gmail.com>
2025-02-13 22:46:52 +0530

    changed some configurations of css and others

---
+++ app/(ROOT)/utils/helper_data.ts
@@ -0,0 +1,63 @@
+export interface Category{
+    title: string;
+    url: string;
+    icon: any;
+}
+
+export interface NavComponent{
+    title: string,
+    url: string,
+    icon: any
+}
+
+export class Helper{
+
+    public static basePath:string = 'http://localhost:3000'
+    
+    public static categories():Category[] {
+        const categories = [
+            {
+                title: "Learning & Skills",
+                url: `/categories/${encodeURIComponent('Learning & Skills')}`,
+                icon: 'skill'
+            },
+            {
+                title: "Mindset & Focus",
+                url: `/categories/${encodeURIComponent('Mindset & Focus')}`,
+                icon: 'mindset'
+            },
+            {
+                title: "Descision Making & Strategy",
+                url: `/categories/${encodeURIComponent('Descision Making & Strategy')}`,
+                icon: 'strategy'
+            },
+            {
+                title: "Industry Trends",
+                url: `/categories/${encodeURIComponent('Industry Trends')}`,
+                icon: 'trends'
+            },
+            {
+                title: "Health & Wellness",
+                url: `/categories/${encodeURIComponent('Health & Wellness')}`,
+                icon: 'health'
+            },
+            {
+                title: "Career Growth",
+                url: `/categories/${encodeURIComponent('Career Growth')}`,
+                icon: 'growth'
+            }
+        ];
+        return categories as Category[];
+    }
+
+    public static navComponents():NavComponent[] {
+        const navs = [
+            {
+                'title': 'dashboard',
+                'url': `/navs/dashboard`,
+                'icon': 'dashboard'
+            }
+        ];
+        return navs as NavComponent[];
+    }
+}
