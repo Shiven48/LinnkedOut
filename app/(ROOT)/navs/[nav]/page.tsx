e5f737c2e3e2a28b8f6f276ab0e4d35abcc932ef
Shiven48 <shevenroyal@gmail.com>
2025-02-17 22:57:36 +0530

    updated helper data

--- app/(ROOT)/navs/[nav]/page.tsx
+++ app/(ROOT)/navs/[nav]/page.tsx
@@ -3,6 +3,8 @@ export default async function Page({params}:{params:{nav:string}}){
     const decodedNav = decodeURIComponent(nav);
     
     return(
-        <span className="bg-red-300 text-black"> Hello this is Nav:{decodedNav} </span>
+        <div className="bg-red-950 w-full h-full flex justify-center items-center">
+            <div className="text-black w-1/2 h-1/2 text-center content-center bg-white"> This is {decodedNav} page </div>
+        </div>
     )
 }
