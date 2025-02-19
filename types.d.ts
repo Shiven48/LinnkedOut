7278a396dc6d6e86eb5dc4f5d699a5989fbc7a3a
Shiven48 <shevenroyal@gmail.com>
2025-02-19 22:34:09 +0530

    Added types of the entities

--- types.d.ts
+++ types.d.ts
@@ -11,3 +11,32 @@ export interface Resource {
     id: number,
     link: string
 }
+
+export interface Media {
+    id?: number;
+    type: 'short' | 'image' | 'video';
+    platform: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface YoutubeMedia {
+    id?: number;
+    mediaId?: number;
+    videoId: string;
+    title: string,
+    description: string | null;
+    thumbnailUrl: string | null;
+    thumbnailMediumUrl: string | null;
+    thumbnailHighUrl: string | null;
+    duration: string | null;
+    definition: string | null;
+    hasCaption: boolean | null;
+    tags: string[] | null;
+}
+
+export interface MediaRelations {
+    youtubeDetails?: YoutubeMedia;  
+    twitterDetails?: TwitterMedia;  
+    instagramDetails?: InstagramMedia;
+  }
