0122bd65749dccf7bd018e135476f1cba2eeeb0d
Shiven48 <shevenroyal@gmail.com>
2025-02-20 23:57:33 +0530

    added parsing function for the twitter embedded links

--- src/app/(ROOT)/_lib/helper_funcs.ts
+++ src/app/(ROOT)/_lib/helper_funcs.ts
@@ -10,9 +10,18 @@ export class HelperFunctions{
     }
 
     public static parseYoutubeEmbeddedLink(link:string){
+        // change here -> currently i am using data that i have declared not using the data provided by the bot 
         const url = new URL(Helper.Resources()[0].link).pathname
         const trimmedUrl = url.trim()
         return trimmedUrl.split('/')[1]
+    }
+
+    public static parseTwitterEmbeddedLink(link:string) {
+        if(!link) throw new Error('Cannot parse the url check the url again')
+        return new URL(link).pathname
+                .split('/')
+                .pop()
+                ?.trim()
     }
 
     public static youtubeVideoDetails(id:string){
