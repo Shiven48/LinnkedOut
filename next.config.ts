6e260d40fdf91723b8e7535e15790d464f10c936
Shiven48 <shevenroyal@gmail.com>
2025-02-25 23:09:48 +0530

    Added styling for the homepage card component

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['external-preview.redd.it', 'i.redd.it', 'preview.redd.it', 'i.ytimg.com'],
+  },
 };
 
 export default nextConfig;
