380dcaecf473123605e8b99a9b84dc6893e4b709
Shiven48 <shevenroyal@gmail.com>
2025-02-25 23:09:48 +0530

    Added styling for the homepage card component

--- src/app/(HOME)/page.tsx
+++ src/app/(HOME)/page.tsx
@@ -1,30 +1,29 @@
 'use client'
 import { useEffect, useState } from "react";
 import { useSidebarState } from "../../../hooks/useSideBarState";
-import Trial from "../_components/Trial";
 import { Media } from "../../../types";
 import Image from "next/image";
 import Card from "../_components/Card";
 
 export default function Home() {
     // const isOpen = useSidebarState(state => state.isOpen) 
-    const [media,setMedia] = useState<Media[]>([])
+    const [media, setMedia] = useState<Media[]>([])
     const [error, setError] = useState<string>('')
 
     useEffect(() => {
         const fetchedMedia = async () => {
             try {
                 const res = await fetch('/api/videos');
-                if (!res.ok) {  
+                if (!res.ok) {
                     throw new Error(`Error: ${res.statusText}`);
                 }
                 const data = await res.json();
                 setMedia(data.body);
-            } catch(error: unknown) {
+            } catch (error: unknown) {
                 console.error('Error fetching media:', error);
                 setError(error instanceof Error ? error.message : 'An error occurred');
             }
-        }   
+        }
         fetchedMedia();
     }, []);
 
@@ -35,12 +34,11 @@ export default function Home() {
             <div className="flex justify-evenly mt-10 flex-wrap cursor-pointer">
                 {media.length > 0 ? (
                     media.map((video: Media) => (
-                        <div 
+                        <div
                             key={video.id}
-                            className="relative bg-golden w-[30%] h-72 p-4 rounded-xl shadow-sm shadow-white border border-black m-4 
-                            transition 1s ease-out flex flex-col items-center hover-page-card"
+                            className="card-green-glass relative w-[30%] h-72 p-4 m-4 flex flex-col items-center"
                         >
-                            <Card media={video}/>
+                            <Card media={video} />
                         </div>
                     ))
                 ) : (
