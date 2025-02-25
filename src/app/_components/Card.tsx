959cbe97f6f7933c67c65e29debc8ce9bbe437dc
Shiven48 <shevenroyal@gmail.com>
2025-02-25 23:11:16 +0530

    Changed reddit service to handle thumbnail url fetching and storing properly

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -1,18 +1,20 @@
+import Image from 'next/image';
 import { Media } from "../../../types";
 
 export default function Card({ media }: { media: Media }){
     return(
         // This styling is just temporary
-        <div className="text-black m-2 font-serif font-thin flex gap-4 absolute bottom-4 items-center justify-end">
-        {/*<Image
-            src=""
-            alt=""
-            width={0}
-            height={0}
-            /> */}
-            <p className="w-20 h-6 bg-green-400 rounded-xl flex justify-center border border-black">{media.platform}</p>
-            <p className="w-20 h-6 bg-orange-400 rounded-xl flex justify-center border border-black"> {media.type}</p>
-            <p className="w-36 h-6 bg-gray-400 rounded-xl flex justify-center border border-black"> {new Date(media.createdAt).toDateString()}</p>
+        <div className="text-black m-2 font-serif font-thin flex gap-4 absolute bottom-4 items-center">
+            <Image
+                src={media.thumbnailUrl}
+                alt="Media thumbnail"
+                width={100}
+                height={100}
+                style={{ objectFit: 'cover' }}
+            />
+            <span className="px-1 bg-green-400 rounded-xl flex justify-center border border-black">{media.platform}</span>
+            <span className="px-1 bg-orange-400 rounded-xl flex justify-center border border-black"> {media.type}</span>
+            <span className="px-1 bg-gray-400 rounded-xl flex justify-center border border-black"> {new Date(media.createdAt).toDateString()}</span>
         </div>
     )
 }
