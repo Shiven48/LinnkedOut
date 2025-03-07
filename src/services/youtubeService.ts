a45fe945a84880841c5e084a85456639aa3bd0cf
Shiven48 <shevenroyal@gmail.com>
2025-03-08 02:18:00 +0530

    changed orm functions in media file

--- src/services/youtubeService.ts
+++ src/services/youtubeService.ts
@@ -1,39 +1,45 @@
-import { HelperFunctions } from "../../src/app/_lib/helper_funcs";
-import { Media, YoutubeMedia } from "../../types";
-import { NextResponse } from "next/server"
-import { insertMedia, insertYoutubeMedia } from "../server/functions/media";
+// API Route to fetch video metadata from YouTube
+import { HelperFunctions } from "../../src/app/_lib/helper_funcs"; 
+import { Media, YoutubeMedia } from "../../types"; 
+import { NextResponse } from "next/server"; 
+import { insertMedia, insertYoutubeMedia } from "../server/functions/media"; 
+import { revalidatePath } from "next/cache"; 
 
-export const fetchVideoFromYoutubeURL = async (link:string) => {
-    try {
-        const videoId = HelperFunctions.parseYoutubeEmbeddedLink(link);
-        if(!videoId){
-            return NextResponse.json({message:'Unable to fetch videoId'},{status:500})
+export const fetchVideoFromYoutubeURL = async (link: string) => { 
+    try { 
+        const videoId = HelperFunctions.parseYoutubeEmbeddedLink(link); 
+        if (!videoId) { 
+            return NextResponse.json({ message: 'Unable to fetch videoId' }, { status: 500 }); 
         }
-
-        // process here the youtube dlp function
-        // HelperFunctions.accessDLP(videoId)
         
-        if(!process.env.YOUTUBE_API_KEY){
-            return NextResponse.json({message:'Unable to fetch environment keys'},{status:400}) 
+        if (!process.env.YOUTUBE_API_KEY) { 
+            return NextResponse.json({ message: 'Unable to fetch environment keys' }, { status: 400 }); 
         }
+        
         const youtubeKey = process.env.YOUTUBE_API_KEY;
-        const fetchedVideo = await fetch(`https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${youtubeKey}&part=snippet,contentDetails,statistics,status`, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-        if (!fetchedVideo.ok) {
-            return NextResponse.json({message:'Unable to fetch video metadata'},{status:400}) 
+        const fetchedVideo = await fetch(
+            `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${youtubeKey}&part=snippet,contentDetails,statistics,status`,
+            { method: 'GET', headers: { 'Content-Type': 'application/json' } }
+        );
+        
+        if (!fetchedVideo.ok) { 
+            return NextResponse.json({ message: 'Unable to fetch video metadata' }, { status: 400 }); 
         }
+        
         const videoMetaData = await fetchedVideo.json();
-        return videoMetaData
-        // return saveToDatabase(videoMetaData)
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
-}
+        
+        const savedData = await saveToDatabase(videoMetaData)
+        if (!savedData) { 
+            return NextResponse.json({ message: 'Error saving to the database' }, { status: 500 }); 
+        }
+        
+        revalidatePath('/'); 
+        return savedData
+    } catch (error) { 
+        console.error(error); 
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 }); 
+    } 
+};
 
 export const saveToDatabase = async (videoMetaData:any) => {
     const { items } = videoMetaData;
