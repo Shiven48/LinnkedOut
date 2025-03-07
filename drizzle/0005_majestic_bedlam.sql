4ce8bec36f9ba354b1e2c5cbad69d34d65eb3549
Shiven_Royal <shevenroyal@gmail.com>
2025-03-08 02:45:13 +0530

    Delete drizzle directory

--- drizzle/0005_majestic_bedlam.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "reddit" ADD COLUMN "videoHeight" integer;
--- drizzle/0010_easy_bastion.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "media" ADD COLUMN "hdImageUrl" varchar(200);--> statement-breakpoint
-ALTER TABLE "reddit" ADD COLUMN "hdImageUrl" text;
