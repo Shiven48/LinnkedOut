f71d12beac31e9a9f0e2200db4e49553e2c3b054
Shiven48 <shevenroyal@gmail.com>
2025-03-08 15:08:00 +0530

    Added Loading component for homepage

---
+++ FE/src/app/_components/Loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return(
+        <div className="h-100vh w-full overflow-y-auto bg-dark flex">
+            <div className="absolute top-[50%] left-[55%] rounded-full w-8 h-8 animate-spin border-4 border-white border-t-transparent">
+        </div>
+        </div>        
+    )
+}
---
+++ FE/src/app/_components/NoContent.tsx
@@ -0,0 +1,14 @@
+export default function NoContent() {
+    return(
+        <p className="text-dark-golden">
+            Oops! Looks like this page is as empty as the{' '}
+            <a 
+                href="https://en.wikipedia.org/wiki/Bo%C3%B6tes_Void"
+                className="text-blue-400 hover:text-blue-600"
+            >
+                Boötes Void
+            </a>
+            . Check back later for something awesome!
+        </p>
+    )
+}
