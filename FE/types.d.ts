51a50619e255fd09bd217e8c5849c0ac35ec5c97
Shiven48 <shevenroyal@gmail.com>
2025-03-14 18:46:30 +0530

    Added more types for reddit comments and yt transcripts

--- FE/types.d.ts
+++ FE/types.d.ts
@@ -12,83 +12,69 @@ export interface Resource {
     link: string
 }
 
-export interface Media {
-    id?: number;
-    type: 'short' | 'image' | 'video' | 'photo';
-    platform: string;
-    createdAt: string;
-    updatedAt: string;
-    thumbnailUrl: string;
-    hdThumbnailUrl?: string;
-    title: string;
-    duration_ms?: string;
-}
-
-export interface YoutubeMedia {
-    id?: number;
-    mediaId?: number;
-    videoId: string;
-    title: string,
-    description: string | null;
-    thumbnailUrl: string | null;
-    thumbnailMediumUrl: string | null;
-    thumbnailHighUrl: string | null;
-    thumbnailMaxRes?: string | null;
-    duration_ms?: string | null;
-    definition: string | null;
-    hasCaption: boolean | null;
-    tags: string[] | null;
-}
-
-export interface TwitterMedia {
-    id?:number;
-    mediaId?: number;
-    tweetId: string;
-    text: string | null;
-    tweet_media_key: string; 
-    media_url: string;
-    username: string | null;
-    duration_ms: string;
-    // varients: Varient
-}
-
-export interface RedditMedia{
-    id?: number;
-    mediaId?: number;
-    subreddit: string;
-    title: string;
-    type: string;
-    redditPostId: string;
-    author: string;
-    imageUrl: string;
-    hdImageUrl: string | null;
-    imageWidth: number | null;
-    imageHeight: number | null;
-    videoUrl: string;
-    videoWidth: number | null;
-    videoHeight: number | null;
-    duration_ms?: string | null
-} 
-
-export interface Varient {
-    type: string;
-    url: string;
-    bitrate: number;
-}
-
-export interface MediaRelations {
-    youtubeDetails?: YoutubeMedia;  
-    twitterDetails?: TwitterMedia;  
-    instagramDetails?: InstagramMedia;
-  }
-
-export interface Urls {
-    sdUrl?: string;
-    hdUrl?: string;
-}
-
 export interface Platfrom {
     name: string, 
     url: string,
     icon: string
+}
+
+// The extra data for platform schemas(YT, Reddit etc)
+export interface CaptionItem {
+    text: string,
+    duration: number,
+    offset: number,
+    lang: string
+}
+
+interface CommentData {
+    body: string;
+    score: number;
+    author: string;
+    ups: number;
+    downs: number;
+    replies?: CommentData[];
+}
+
+export interface RedditComment {
+    data: {
+        body: string;
+        score: number;
+        author: string;
+        ups: number;
+        downs: number;
+        replies?: any;
+    };
+}
+
+// The below types are for database schema objects
+export interface Media {
+    id?: number;                                     // This is the id of the media in the database (This id is acting FK in other schemas)
+    type: 'short' | 'image' | 'video' | 'photo';
+    platform: string;                                // This will be any of reddit | youtube | or any other platform
+    thumbnailUrl?: string;                           // This is the thumbnail url
+    postUrl: string;                                 // This is the video | image url
+    title: string;                      
+    durationMs?: number;                             // This is optional because c'mon images dont have duration
+    postId: string;                                  // The id of the post on that platform
+}
+
+export interface RedditMedia {
+    id?: number;                                    // This is the id of the media in the database
+    mediaId?: number;                               // This ID is referring to the Media schema
+    subreddit: string;                  
+    author: string;                                 // Author of the post
+    postLink: string;                               // This is the link to the thread on reddit
+    comments?: CommentData[];                       // High level comments and the nested ones
+}
+
+export interface YoutubeMedia {
+    id?: number;                                    // This is the id of the media in the database
+    mediaId?: number;                               // This is referring to the Media schema
+    description?: string | null;                    // Description of the video
+    definition?: string | null;                     // The definition of the video
+    englishCaptions?: CaptionItem[];                // Link to the captions
+}
+
+export interface MediaRelations {
+    youtubeDetails?: YoutubeMedia;  
 }
