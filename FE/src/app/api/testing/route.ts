86d78e5b6aebc74cf3f90d17763983b0564cfe98
Shiven48 <shevenroyal@gmail.com>
2025-03-14 18:44:55 +0530

    Updated testing route

--- FE/src/app/api/testing/route.ts
+++ FE/src/app/api/testing/route.ts
@@ -1,21 +1,17 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-import { fetchVideoFromYoutubeURL } from '../../../services/youtubeService';
 import { Helper } from '../../_lib/helper_data';
 import { NextRequest, NextResponse } from 'next/server';
-import { fetchVideoFromTwitterURL } from '../../../services/twitterService';
 import { fetchVideoFromRedditURL } from '../../../services/redditService';
+import { fetchVideoFromYoutubeURL } from '../../../services/youtubeService'
 import { revalidatePath } from 'next/cache';
 
 export async function GET(
     req: NextRequest
 ) {
     try {
-        const mappedVideo = await fetchVideoFromYoutubeURL(Helper.Resources()[13].link)   // ->    For Youtube
-        // const mappedVideo = await fetchVideoFromTwitterURL(Helper.Resources()[3].link)      // ->    For Twitter
-        // const mappedVideo = await fetchVideoFromRedditURL(Helper.Resources()[11].link)    // ->    For Reddit
+        const mappedVideo = await fetchVideoFromYoutubeURL(Helper.Resources()[11].link)        // ->    For Youtube
+        // const mappedVideo = await fetchVideoFromRedditURL(Helper.Resources()[3].link)             // ->    For Reddit
         revalidatePath('/'); 
-        return NextResponse.json({ body:mappedVideo, status: 200});
-        // return NextResponse.json(mappedVideo)        
+        return NextResponse.json({ body:mappedVideo, status: 200});       
     } catch (error: any) {
         console.error("Caught error in GET handler:", error.message);
         return new NextResponse(
