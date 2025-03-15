7ad3aec5065abda0f2d9491fdd4ede830f8b7b49
Shiven48 <shevenroyal@gmail.com>
2025-03-15 22:43:02 +0530

    Added snapshot files of drizzle schema

--- FE/drizzle.config.ts
+++ FE/drizzle.config.ts
@@ -1,16 +1,26 @@
-import { Config } from 'drizzle-kit'
-import * as dotenv from 'dotenv'
-dotenv.config({ path: '.env.local'})
+import { Config } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
 
-if(!process.env.DATABASE_URL) {
-    throw new Error('Database URL is missing')
+dotenv.config({ path: '.env.local' });
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is missing');
 }
+if (!process.env.REMOTE_DATABASE_URL) {
+    throw new Error('REMOTE_DATABASE_URL is missing');
+}
+
+const localDbUrl: string = process.env.DATABASE_URL;
+const remoteDbUrl: string = process.env.REMOTE_DATABASE_URL;
+
+const isProduction = process.env.NODE_ENV === 'production';
+const databaseUrl = isProduction ? remoteDbUrl : localDbUrl;
 
 export default {
     schema: './src/server/db/schema.ts',
     out: './drizzle',
     dialect: 'postgresql',
     dbCredentials: {
-      url: process.env.DATABASE_URL!,
+        url: databaseUrl,
     },
 } satisfies Config;
