de3fa2eaa498f7d46794f82c6d873bb56e84e74a
Shiven48 <shevenroyal@gmail.com>
2025-03-16 04:23:19 +0530

    updated home page

--- FE/src/app/(HOME)/page.tsx
+++ FE/src/app/(HOME)/page.tsx
@@ -32,7 +32,34 @@ export default function Home() {
         }
         fetchedMedia();
     }, []);
+
+    const viewTime = (durationMs:number):string => {
+        const duration = durationMs / 1000;
+        const hours = Math.floor(duration / 3600); 
+        const minutes = Math.floor((duration % 3600) / 60);
+        const seconds = Math.floor(duration % 60);
+
+    if(hours < 1){
+        return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;    
+    }
+    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
     
+    const getPlatformIcon = (platform:string) => {
+        switch (platform.toLowerCase()) {
+          case 'reddit': return '/reddit.svg'; 
+          case 'youtube': return '/youtube.svg';
+          default: return '🎬';
+        }
+      };
+
+      const getTitle = (title:string):string => {
+        if(title.length > 40){
+            return title.slice(0,50).concat('...')
+        }
+        return title;
+      }
+
     // Return the Loading component when data is still loading
     if(isLoading) {
         return <Loading />
@@ -45,7 +72,7 @@ export default function Home() {
                     media.map((video: Media) => (
                         <div
                             key={video.id}
-                            className="card-green-glass w-[30%] h-53 my-8"
+                            className="card-green-glass w-[36%] h-64 my-8"
                         >
                             <div className="flex gap-4 justify-center">
                                 <div className='aspect-video -z-90 rounded-2xl overflow-hidden'>
@@ -67,9 +94,28 @@ export default function Home() {
                                     </Link>
                                 </div>
                             </div>
-                            <div className="span-prop relative flex top-54 rounded-[5px]">
-                                <div>
-                                    <span>{video.platform}</span>
+                            <div className="span-prop relative flex top-68 rounded-[5px] text-white">
+                            <div className="w-10 h-6 absolute -top-14 end-2 bg-black bg-opacity-80 rounded text-center text-sm">
+                                <span className="text-white flex items-center justify-center pt-1">{viewTime(video.durationMs)}</span>
+                            </div>
+                            <div className="w-10 h-6 absolute -top-64 start-2 bg-darker rounded text-center text-sm">
+                                <span className="text-golden flex items-center justify-center pt-1">HD</span>
+                            </div>
+                                <div className="flex flex-col justify-evenly">
+                                <span className="text-large text-white -mt-2">{getTitle(video.title)}</span>
+                                <div className="flex items-center mb-1.5 justify-evenly">
+                                    <div className="flex items-center justify-evently bg-opacity-70 bg-gray-800 rounded-full px-2 py-0.5 border border-white absolute -top-14 start-2">
+                                        <Image 
+                                            src={getPlatformIcon(video.platform)}
+                                            width={16}
+                                            height={16}
+                                            alt={video.platform}
+                                            className="mr-1"
+                                        />
+                                        <span className="text-xs font-medium text-gray-300">{video.platform}</span>
+                                    </div>    
+                                </div>
+                                <span className="text-medium text-gray-400">{video.type}</span>
                                 </div>
                             </div>
                         </div>
