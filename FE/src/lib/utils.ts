db8d44ed6a839db3f86c54666550dfc8972dbcd6
Shiven48 <shevenroyal@gmail.com>
2025-04-05 23:58:39 +0530

    Moved all the utils functions in the utils file

--- FE/src/lib/utils.ts
+++ FE/src/lib/utils.ts
@@ -1,6 +1,21 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { ErrorObj } from "../../types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function HandleError(error:any){
+  if (error instanceof Error) {
+    console.error("Error fetching videos:", error);
+    const errorObj:ErrorObj = { 
+      name: error.name, 
+      message: error.message 
+    };
+    return errorObj
+  } else {
+    console.warn("Caught an unknown error type:", error);
+    return error
+  }
 }
