3a9742296525a43bb360e78c9b2d33151a1a4c4d
DhrishP <parekhdhrish.pg@gmail.com>
2025-09-13 19:07:41 +0530

    migrate : db and remove : redis

--- FE/src/server/db/index.ts
+++ FE/src/server/db/index.ts
@@ -1,30 +1,16 @@
-import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js"
-import postgres from 'postgres'
-import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import * as schema from './schema';
-
+import postgres from 'postgres';
 config({ path: ".env.local" });
 
 if (!process.env.REMOTE_DATABASE_URL) {
     throw new Error('REMOTE_DATABASE_URL is missing');
 }
 
-if(!process.env.LOCAL_PG_DATABASE){
-    throw new Error('DATABASE_URL is missing');
-}
+const remoteClient = postgres(process.env.REMOTE_DATABASE_URL!);
 
-const localClient = postgres(process.env.LOCAL_PG_DATABASE!);
-const remoteClient = neon(process.env.REMOTE_DATABASE_URL!);
+const db = drizzlePostgres(remoteClient as any, { schema })
 
-const isProduction = process.env.NODE_ENV === "production";
-let db: any;
-
-if(isProduction){
-    db = drizzleNeon(remoteClient, { schema }) 
-} else{
-    db = drizzlePostgres(localClient, { schema })
-}
 export * from './schema'
 export { db }
