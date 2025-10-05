e24edb09a47c2f223386fc757bc12279710b9109
DhrishP <parekhdhrish.pg@gmail.com>
2025-10-05 11:34:41 +0530

    fix : auth issues and add : button

--- FE/next.config.ts
+++ FE/next.config.ts
@@ -1,35 +1,34 @@
-
-import type { NextConfig } from "next"
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    // config options here
-    reactStrictMode: true,
-    eslint: {
-        ignoreDuringBuilds: true, // Disable ESLint during builds
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'external-preview.redd.it'
-            },
-            {
-                protocol: 'https',
-                hostname: 'i.redd.it'
-            },
-            {
-                protocol: 'https',
-                hostname: 'preview.redd.it'
-            },
-            {
-                protocol: 'https',
-                hostname: 'i.ytimg.com'
-            }
-        ]
-    }
-}
+  // config options here
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during builds
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "external-preview.redd.it",
+      },
+      {
+        protocol: "https",
+        hostname: "i.redd.it",
+      },
+      {
+        protocol: "https",
+        hostname: "preview.redd.it",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
