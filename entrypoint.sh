4f540da924e797f902c45581f05eb614a308d54d
Shiven_Royal <shevenroyal@gmail.com>
2026-01-04 18:04:45 +0530

    fix: Changed the path of youtube-cookies to /app/youtube-cookies.txt

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,14 @@ if [ -z "$YOUTUBE_COOKIES" ]; then
     echo "Warning: YOUTUBE_COOKIES variable is empty."
 else
     # Use the /app/temp directory because it has the correct permissions
-    printf "%s" "$YOUTUBE_COOKIES" > /app/temp/youtube-cookies.txt
+    printf "%s" "$YOUTUBE_COOKIES" > /app/youtube-cookies.txt
     if [ $? -eq 0 ]; then
-        echo "Cookies file successfully written to /app/temp/youtube-cookies.txt"
+        echo "Cookies file successfully written to /app/youtube-cookies.txt"
     else
         echo "FAILED to write cookies file."
     fi
 fi
 
-# Ensure HOSTNAME is set to 0.0.0.0 to fix EADDRNOTAVAIL
 export HOSTNAME=0.0.0.0
 
 exec node server.js
